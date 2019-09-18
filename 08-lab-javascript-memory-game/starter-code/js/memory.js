5a4a84061a7c6cffd2f74510e4adc53c6214de24
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-18 12:44:23 -0300

    added lab 08, memory game

---
+++ 08-lab-javascript-memory-game/starter-code/js/memory.js
@@ -0,0 +1,28 @@
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
+  shuffleCards() {
+    for (let i = 0; i < this.cards.length - 2; i++) {
+      let j = Math.floor(Math.random() * (this.cards.length - 1 - i + 1) + i);
+      let temp = this.cards[j];
+      this.cards[j] = this.cards[i];
+      this.cards[i] = temp;
+    }
+  }
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 == card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  isFinished() {
+    return this.pairsGuessed === this.cards.length / 2 ? true : false;
+  }
+}
